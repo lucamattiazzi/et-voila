b77ba38dca091d577f87859b59567fa26cff794b
Luca Mattiazzi <l.d.mattiazzi@gmail.com>
2020-06-22 00:48:05 +0200

    name

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,4 +1,5 @@
 declare type Callback = (element?: Node, error?: Error) => any;
 declare type Remove = () => void;
+export declare function waitForIt(selector: string, timeout?: number): Promise<Node>;
 export declare function waitForIt(selector: string, callback: Callback, timeout?: number): Remove;
 export {};
--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-function waitForIt(selector, callback, timeout) {
+function isCallback(variable) {
+    return variable && typeof variable === 'function';
+}
+function waitForItCb(selector, callback, timeout) {
     const observer = new MutationObserver(records => {
         for (const record of records) {
             const { addedNodes } = record;
@@ -25,6 +28,37 @@ function waitForIt(selector, callback, timeout) {
     }
     const remove = () => observer.disconnect();
     return remove;
+}
+function waitForItPromise(selector, timeout) {
+    return new Promise((resolve, reject) => {
+        const observer = new MutationObserver(records => {
+            for (const record of records) {
+                const { addedNodes } = record;
+                if (!addedNodes || addedNodes.length === 0)
+                    continue;
+                for (const node of Array.from(addedNodes)) {
+                    const parent = node.parentElement;
+                    if (!parent)
+                        continue;
+                    if (!parent.querySelector(selector))
+                        continue;
+                    resolve(node);
+                }
+            }
+        });
+        observer.observe(document.body, { childList: true, attributes: true, subtree: true });
+        if (timeout) {
+            setTimeout(() => {
+                observer.disconnect();
+                reject();
+            }, timeout);
+        }
+    });
+}
+function waitForIt(selector, callback, timeout) {
+    if (isCallback(callback))
+        return waitForItCb(selector, callback, timeout);
+    return waitForItPromise(selector, callback);
 }
 exports.waitForIt = waitForIt;
 //# sourceMappingURL=index.js.map
